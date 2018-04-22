02e416dfd6241e3b4b4e1ecf881ddd3a7e271134
Thanos Doumanis <doumanis@gmail.com>
2018-04-22 20:40:52 +0300

    post apis connecting to the back-end server

---
+++ readable-frontend/src/utils/readableAPI.js
@@ -0,0 +1,58 @@
+//Locall server must be running
+const api = "http://localhost:3001"
+
+
+//Generate a unique token for use with the backend server.
+let token = localStorage.token
+if (!token)
+  token = localStorage.token = Math.random().toString(36).substr(-8)
+
+const headers = {
+  'Accept': 'application/json',
+  'Authorization': token
+}
+
+//Get all available gategories from server
+export const categories = () =>
+  fetch(`${api}/categories`, { headers })
+    .then(res => res.json())
+    .then(data => data.categories)
+
+// Get all posts from server
+export const allPosts = () =>
+  fetch(`${api}/posts`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
+// Add a new post to server
+export const addNewPost = (post) =>
+  fetch(`${api}/posts`, {
+    method: 'POST',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify( post )
+  }).then(res => res.json())
+
+// Delete a post from the server
+export const deletePost = (id) =>
+  fetch(`${api}/posts/${id}`, {method: 'DELETE', headers})
+    .then(res => res.json())
+
+// Gat a specific post
+export const getPost = (id) =>
+  fetch(`${api}/posts/${id}`, { headers })
+    .then(res => res.json())
+    .then(data => data)
+
+// Update a specific post
+export const updatePost = (id, title, body) =>
+  fetch(`${api}/posts/${id}`, {
+    method: 'PUT',
+    headers: {
+      ...headers,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({title, body})
+  }).then(res => res.json())
