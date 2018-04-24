8ef2a5b8d872143e96e4fabd8a26ee82ec89de71
Thanos Doumanis <doumanis@gmail.com>
2018-04-24 17:39:54 +0300

    create error page

---
+++ readable-frontend/src/components/Error.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Error = () => (
+  <div className="errorPage">
+    <h2>Error 404: Page not found :-(</h2>
+    <p>The page you are looking for does not exist.<br/>Please use the menu to navigate.</p>
+  </div>
+)
+
+export default Error
