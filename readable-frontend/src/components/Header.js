16f1435d8edcf32f9a87d46555979aa9f06b07bc
Thanos Doumanis <doumanis@gmail.com>
2018-04-24 17:30:47 +0300

    create router navlinks in the menu
    change home menu to an icon

--- readable-frontend/src/components/Header.js
+++ readable-frontend/src/components/Header.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {capitalize} from '../utils/helper'
+import {capitalize} from '../utils/helper';
+import { NavLink } from 'react-router-dom';
+import Home from 'react-icons/lib/fa/home';
 
 class Header extends Component  {
 
@@ -10,16 +12,15 @@ class Header extends Component  {
 
   render() {
     const {categories} = this.props;
-
     return (
       <header>
         <h1 className="logo">Readable</h1>
         <p className="subtitle">Everythging worth reading</p>
         <nav>
           <div className="menu">
-            <a className="menu-item" href="/">Home</a>
+            <NavLink exact to='/' className="menu-item"><Home /></NavLink>
             {categories.map((category, index) => (
-              <a className="menu-item" href={`/${category.path}`} key={index}>{capitalize(category.name)}</a>
+              <NavLink to={`/${category.path}`} className="menu-item" key={index}>{capitalize(category.name)}</NavLink>
             ))}
           </div>
         </nav>
