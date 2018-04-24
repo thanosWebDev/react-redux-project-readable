99027037179207b2cfa6266652fbc322f8ad9c30
Thanos Doumanis <doumanis@gmail.com>
2018-04-24 17:24:47 +0300

    add Reacr Router to the app

--- readable-frontend/src/index.js
+++ readable-frontend/src/index.js
@@ -4,6 +4,7 @@ import './css/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
 import reducer from './reducers';
 import { Provider } from 'react-redux'
 
@@ -22,6 +23,10 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(
-<Provider store={store}><App /></Provider>, document.getElementById('root'));
+<Provider store={store}>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+</Provider>, document.getElementById('root'));
 
 registerServiceWorker();
