374f4098f83c699f8692ee66ae446d090ef44102
Thanos Doumanis <doumanis@gmail.com>
2018-04-29 21:48:06 +0300

    setup thunk

--- readable-frontend/src/index.js
+++ readable-frontend/src/index.js
@@ -4,6 +4,7 @@ import './css/index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { BrowserRouter } from 'react-router-dom';
 import reducer from './reducers';
 import { Provider } from 'react-redux'
@@ -20,7 +21,7 @@ const logger = store => next => action => {
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducer, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk,logger)));
 
 ReactDOM.render(
 <Provider store={store}>
