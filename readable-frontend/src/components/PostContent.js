1cb12f915e3af221e9e6ab327187a3a22fd850d4
Thanos Doumanis <doumanis@gmail.com>
2018-04-30 02:59:56 +0300

    new presentation component

--- readable-frontend/src/components/PostContent.js
+++ readable-frontend/src/components/PostContent.js
@@ -1,23 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import Vote from './Vote';
-import PostActions from './PostActions'
 import {capitalize, dateConvert} from '../utils/helper'
 
-class Post extends Component  {
+class PostContent extends Component  {
   static propTypes = {
     post: PropTypes.object.isRequired,
+    openModal: PropTypes.func.isRequired,
+    deletePost: PropTypes.func.isRequired
   }
 
   render() {
-    const {post} = this.props;
+    const {post, openModal, deletePost} = this.props;
     return (
-        <div className="post">
-          <Vote votes={post.voteScore}
-                id={post.id}
-                voteRole={"post"}
-          />
           <div className="postContent">
             <div className="postInfo"><span className="greenText">{capitalize(post.author)}</span> • {dateConvert(post.timestamp)} • {capitalize(post.category)}</div>
             <Link to={`/${post.category}/${post.id}`}>
@@ -25,11 +20,13 @@ class Post extends Component  {
             </Link>
             <p className="excerpt">{post.body.substr(0, 130)}…</p>
             <div className="comments">Comments: {post.commentCount}</div>
-            <PostActions id={post.id}/>
+            <div className="postActions">
+              <div className="edit" onClick={openModal}>Edit</div>
+              <div className="delete" onClick={deletePost}>Delete</div>
+            </div>
           </div>
-        </div>
     )
   }
 }
 
-export default Post
+export default PostContent
