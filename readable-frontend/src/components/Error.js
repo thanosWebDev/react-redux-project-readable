cc758ad3a0cec64a0045deea293ba8a9cf1ae18b
Thanos Doumanis <doumanis@gmail.com>
2018-05-03 02:26:36 +0300

    link to homepage

--- readable-frontend/src/components/Error.js
+++ readable-frontend/src/components/Error.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Error = () => (
   <div className="errorPage">
     <h2>Error 404: Page not found :-(</h2>
     <p>The page you are looking for does not exist.<br/>Please use the menu to navigate.</p>
+    <Link to="/" className="returnLink"> &#8592; Back to homepage</Link>
   </div>
 )
 
